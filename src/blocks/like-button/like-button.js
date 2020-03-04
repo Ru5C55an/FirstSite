3e99cb38673650e41d56f2f0a6163fcc24f4e0b0
Ruslan Sadykov <S.Ru5C55an.N@gmail.com>
2020-03-04 20:21:01 +1000

    Рефакторинг: js код like-button переписан на класс

--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,11 +1,30 @@
+/* eslint-disable no-underscore-dangle */
 import $ from 'jquery';
 
-let clicks = 0;
+class LikeButton {
+  constructor(component) {
+    this.component = component;
+    this.$clicks = $('.js-like-button__counter', this.component).html();
+    this._attachEventHandler();
+    this.$counter = $('.js-like-button__counter', this.component);
+  }
 
-document.querySelector('.like-button__counter').innerHTML = clicks;
+  _attachEventHandler() {
+    $(this.component).on('click', () => {
+      // eslint-disable-next-line eqeqeq
+      if (this.$clicks == 0) {
+        this.$clicks = 1 + +this.$clicks;
+      } else { this.$clicks -= 1; }
 
-$('.like-button').click(() => {
-  clicks += 1;
-  document.querySelector('.like-button__counter').innerHTML = clicks;
-  $('.like-button').addClass('like-button_theme_liked');
+      this.$counter.html(this.$clicks);
+      $(this.component).toggleClass('like-button_theme_liked');
+    });
+  }
+}
+
+$(() => {
+  $('.js-like-button').each((index, node) => {
+    // eslint-disable-next-line no-new
+    new LikeButton(node);
+  });
 });
