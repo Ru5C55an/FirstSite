3e0ea3cd1321a2561fe9df3fe797183e9a78886f
Ruslan Sadykov <S.Ru5C55an.N@gmail.com>
2020-03-15 15:18:49 +1000

    Импортирован JS код блока date-dropdown

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import '../blocks/expandable-checkbox-list/expandable-checkbox-list';
 import '../blocks/range-slider/range-slider';
 import '../blocks/like-button/like-button';
 import '../blocks/datepicker/datepicker';
+import '../blocks/date-dropdown/date-dropdown';
 import $ from 'jquery';
 
 require.context('../blocks/', true, /\.(png|svg|jpg)$/);
